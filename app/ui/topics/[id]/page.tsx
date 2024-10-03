a68c4953b0fd3fef491d1c042f7312532eb72bb6
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-10-02 19:59:01 -0500

    building a next.js app

---
+++ app/ui/topics/[id]/page.tsx
@@ -0,0 +1,5 @@
+export default async function Page({ params }: { params: { id: string } }) {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  return <div>Topic Page: {params.id}</div>;
+}
