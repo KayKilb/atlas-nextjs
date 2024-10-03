ad692b1ebbd16cb3dd0c6c92f3e61ae650dd3ca6
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-10-02 23:07:44 -0500

    layout on page.tsx needed updated

--- app/page.tsx
+++ app/page.tsx
@@ -19,11 +19,11 @@ export default function Page() {
             <div className="flex flex-col gap-2 min-[400px]:flex-row">
               <form>
                 <form
-  action={async () => {
-    "use server";
-    await signIn("default", { redirectTo: "/ui" });
-  }}
->
+                  action={async () => {
+                    "use server";
+                    await signIn("default", { redirectTo: "/ui" });
+                  }}
+                >
                 <button className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50">
                   <div>Sign In</div>
                 </button>
